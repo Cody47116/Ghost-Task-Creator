32399b34749afef5a625495729cd8c9f1e420974
Cody47116 <33329190+Cody47116@users.noreply.github.com>
2018-05-30 22:19:29 -0400

    Update create.js

--- create.js
+++ create.js
@@ -2,19 +2,21 @@ const log      = require('./logger')
 const fs       = require('fs')
 
 let profiles   = JSON.parse(fs.readFileSync('profiles.json', 'utf-8'))
-let config     = JSON.parse(fs.readFileSync('config.json','utf-8'))
-
-let sizes      = [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14]
+let config     = JSON.parse(fs.readFileSync('config.json', 'utf-8'))
 let ratio      = Math.floor(parseInt(config.Accounts) / parseInt(profiles.length))
 let remainer   = parseInt(config.Accounts) % parseInt(profiles.length)
 let totalTasks = [];
 
+log('---------------------------')
+log("-----Made by @Cody_Ncc-----",)
+log('---------------------------')
+log('')
 for (let i = 0; i < profiles.length; i++) {
     if (remainer != 0) {
         for (let b = 0; b != ratio + 1; b++) {
             totalTasks.push({
                 "URL"        : config.Sku,
-                "Size"       : sizes[Math.floor(Math.random() * sizes.length)].toString(),
+                "Size"       : config.sizes[Math.floor(Math.random() * config.sizes.length)].toString(),
                 "Proxy"      : "",
                 "Account"    : "",
                 "Profile"    : `${profiles[i].Name}`,
@@ -30,7 +32,7 @@ for (let i = 0; i < profiles.length; i++) {
         for (let b = 0; b != ratio; b++) {
             totalTasks.push({
                 "URL"        : config.Sku,
-                "Size"       : sizes[Math.floor(Math.random() * sizes.length)].toString(),
+                "Size"       : config.sizes[Math.floor(Math.random() * config.sizes.length)].toString(),
                 "Proxy"      : "",
                 "Account"    : "",
                 "Profile"    : `${profiles[i].Name}`,
@@ -42,6 +44,10 @@ for (let i = 0; i < profiles.length; i++) {
         }
     }
 }
-fs.writeFile(`${config.ExportName}.json`, JSON.stringify(totalTasks), () => {
-    log(`Finished making ${totalTasks.length + remainer} tasks with 1:${ratio} ratio.`, 'success')
+fs.writeFile(`${config.ExportName}.json`, JSON.stringify(totalTasks), (err) => {
+    if (err)
+        log(err, 'error')
+    else
+        log(`Finished making ${totalTasks.length + remainer} tasks with 1:${ratio} ratio.`, 'success')
+
 })
